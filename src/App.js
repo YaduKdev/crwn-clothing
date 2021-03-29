01a4ec2999444bcfb294f5b544f48d3a9a59b8cf
YaduKdev <yaduk.developer@gmail.com>
2021-03-29 07:18:45 +0530

    completed homepage and added shoppage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
 import Homepage from './pages/hompage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-    <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
