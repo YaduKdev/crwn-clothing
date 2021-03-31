eab4042a073a22041905cfc794ceb782764d0741
YaduKdev <yaduk.developer@gmail.com>
2021-03-31 10:45:21 +0530

    add property button to signinwithgoogle button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -39,7 +39,7 @@ class SignIn extends React.Component {
                     <FormInput name='password' type='password' handleChange={this.handleChange} value={this.state.password} label="Password" required />
                     <div className='buttons'>
                         <CustomButton type='submit'>Sign in</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
+                        <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
                     </div>
                 </form>
             </div>
